388f32258d9a7f7b0823c0348968f3ec1fcaa471
anthony <ayomideobaroovooboh@gmail.com>
2026-09-05 21:08:46 +0100

    goo

--- wallet/wallet-controller.js
+++ wallet/wallet-controller.js
@@ -1,11 +1,21 @@
 const walletService=require('./wallet-service')
+const catchAsync=require('../utils/catchAsync')
 
-
-exports.addBalance=async (req,res)=>{
-    try{
-      const wallet= await walletService.addBalance(parseInt(req.params.id),req.body)
-      res.status(200).json({status:'success',data:wallet})
-    }catch(error){
-        res.status(400).json({status:'error', message: error})
-    }
-}
+exports.addBalance= catchAsync(async(req,res,next)=>{
+     try{
+       const response=await walletService.addBalance(req.params.id,req.body,req.user)
+       res.status(200).json({
+        status:'success',
+        data:{
+          wallet: response.wallet,
+          url: response.paystackResponse.data.data.authorizationUrl
+        }
+       })
+     }catch(error){
+        res.status(400).json({
+            status:'fail',
+            message: error
+        })
+     }
+     next()
+})
